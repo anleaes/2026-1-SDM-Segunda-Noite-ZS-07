023f644a45bebee7e828b73b7b33cca4c5cfe3d6
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-22 00:12:48 -0300

    Configure species models

--- apps/species/models.py
+++ apps/species/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Specie(models.Model):
+    name = models.CharField('Nome', max_length=100)
+
+    class Meta:
+        verbose_name = 'Especie'
+        verbose_name_plural = 'Especies'
+        ordering =['id']
+
+    def __str__(self):
+        return f'{self.name}'
