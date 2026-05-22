28356bd449a610849ac68afe4a7a1e4025d99d06
Brandon Viana <brandon.rodrigues.101@gmail.com>
2026-05-21 23:33:54 -0300

    Add initial view setup for Employee model

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Employee
+from rest_framework import viewsets
+from .serializer import EmployeeSerializer
 
 # Create your views here.
