7449c09060892d69826f05c79f7466566f6abee0
Eduardo Kern Goes da Silva <128153817+EduardoKernGoes@users.noreply.github.com>
2026-05-22 00:26:17 -0300

    Revert "Feature/serializer species app"

--- apps/species/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Specie
-from rest_framework import serializers
-
-class SpecieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Specie
-        fields = '__all__'
