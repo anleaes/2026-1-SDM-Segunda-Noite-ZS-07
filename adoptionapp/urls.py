b772dd8ebf9e37322459241f87372fd2b365d8aa
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-22 00:09:36 -0300

    Creates a species app and initial configuration os species models

--- adoptionapp/urls.py
+++ adoptionapp/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pessoas/', include('persons.urls', namespace='persons')),
+    path('especies/', include('species.urls', namespace='species')),
 ]
