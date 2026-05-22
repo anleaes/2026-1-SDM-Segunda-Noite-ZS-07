b4ce2bb11c50a659ee83c425a0f37f90b1fb57d6
Brandon Viana <brandon.rodrigues.101@gmail.com>
2026-05-21 23:34:18 -0300

    Add EmployeeViewSet for handling Employee model CRUD operations

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -5,3 +5,8 @@ from rest_framework import viewsets
 from .serializer import EmployeeSerializer
 
 # Create your views here.
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
