ac2a30a33133da22f00f36900828babcd79a0d18
thaisgmaier <thaisgmaier@oulook.com>
2026-05-22 21:27:43 -0300

    configuring vaccines urls

--- apps/vaccines/urls.py
+++ apps/vaccines/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'vaccines'
+
+router = routers.SimpleRouter()
+router.register('', views.VaccineViewSet, basename='vacinas')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
