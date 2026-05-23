2aba093da5dc6fbe63dbdd3569cdfefaf1caf8cc
thaisgmaier <thaisgmaier@oulook.com>
2026-05-22 21:17:12 -0300

    configure serializer

--- apps/vaccines/serializer.py
+++ apps/vaccines/serializer.py
@@ -1,2 +1,7 @@
 from .models import Vaccine
 from rest_framework import serializers
+
+class VaccineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vaccine
+        fields = '__all__'
