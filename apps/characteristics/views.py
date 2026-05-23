2b7e1c7960fc8ba676227382cbd1f3fdf8ef7824
thaisgmaier <thaisgmaier@oulook.com>
2026-05-22 21:48:09 -0300

    create characteristics views and import components

--- apps/characteristics/views.py
+++ apps/characteristics/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Characteristic
+from rest_framework import viewsets
+from .serializer import CharacteristicSerializer
 
 # Create your views here.
