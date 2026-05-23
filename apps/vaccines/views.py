87e6fb7dcf2db2b389b96734fa7827dfe07fcb8c
thaisgmaier <thaisgmaier@oulook.com>
2026-05-22 21:24:00 -0300

    configure vaccine views class

--- apps/vaccines/views.py
+++ apps/vaccines/views.py
@@ -4,3 +4,6 @@ from rest_framework import viewsets
 from .serializer import VaccineSerializer
 
 # Create your views here.
+class VaccineViewSet(viewsets.ModelViewSet):
+    queryset = Vaccine.objects.all()
+    serializer_class = VaccineSerializer 
