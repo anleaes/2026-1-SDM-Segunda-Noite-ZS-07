aa2300523df27fe6029ba287cb7b0d19050812c9
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-25 18:45:42 -0300

    Configure the adoptions term views

--- apps/adoptionterm/views.py
+++ apps/adoptionterm/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Adoptionterm
+from .serializer import AdoptiontermSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class AdoptiontermViewSet(viewsets.ModelViewSet):
+    queryset = Adoptionterm.objects.all()
+    serializer_class = AdoptiontermSerializer
