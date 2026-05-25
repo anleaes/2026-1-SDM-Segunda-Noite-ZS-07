dc7a5e091616f8c1c78ac9da5f6da24254a61aaf
Brandon Viana <brandon.rodrigues.101@gmail.com>
2026-05-25 17:47:46 -0300

    Implement VaccineItemViewSet for handling VaccineItem model operations

--- apps/vaccineItens/views.py
+++ apps/vaccineItens/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import VaccineItem
+from rest_framework import viewsets
+from .serializer import VaccineItemSerializer
 
 # Create your views here.
+
+
+class VaccineItemViewSet(viewsets.ModelViewSet):
+    queryset = VaccineItem.objects.all()
+    
