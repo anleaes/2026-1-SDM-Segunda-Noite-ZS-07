552cc5bd78b11289bf2e4814a2dae40d5d6521ec
Brandon Viana <brandon.rodrigues.101@gmail.com>
2026-05-25 17:26:04 -0300

    Add app_name declaration for vaccination app URLs

--- apps/vaccination/urls.py
+++ apps/vaccination/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-
+app_name = 'vaccination'
 
 router = routers.SimpleRouter()
 router.register('', views.VaccinationViewSet, basename='vacinacoes')
