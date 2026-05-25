cb964ceb9b7992c560959b28d54c7e763fd035b1
Brandon Viana <brandon.rodrigues.101@gmail.com>
2026-05-25 17:14:36 -0300

    Add Vaccination model with fields and metadata

--- apps/vaccination/models.py
+++ apps/vaccination/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Vaccination(models.Model):
+	vaccinatedAt = models.DateField('Data de Vacinação')
+	weight_at = models.FloatField('Peso na Vacinação', null=True, blank=True)
+	animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+	employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+
+	class Meta:
+		verbose_name = 'Vacinação'
+		verbose_name_plural = 'Vacinações'
+		ordering = ['id']
+
+	def __str__(self):
+		return f"Vacinação {self.id} - {self.animal} em {self.vaccinatedAt}"
