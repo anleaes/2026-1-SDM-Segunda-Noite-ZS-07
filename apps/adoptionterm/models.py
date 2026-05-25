7de5a7dfd91d9069eab39281a1a8910ed05ac989
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-25 15:42:44 -0300

    Configure the adoptions term models

--- apps/adoptionterm/models.py
+++ apps/adoptionterm/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from adoptions.models import Adoption
 
 # Create your models here.
+
+class Adoptionterm(models.Model):
+    number = models.AutoField('Número', primary_key= True, null=False, editable=False, unique=True)
+    adopted_at = models.DateTimeField('Data da Adoção', auto_now_add=True, null=False, editable=False)
+    document = models.FileField('Documento', upload_to='docs', null=False, blank=False)
+    adoption = models.OneToOneField(Adoption, on_delete=models.CASCADE, related_name='adoption_term')
+
+    class Meta:
+        verbose_name = 'Termo de Adocao'
+        verbose_name_plural = 'Termos de Adocao'
+        ordering = ['number']
+
+    def __str__(self):
+        return f'{self.number}'
