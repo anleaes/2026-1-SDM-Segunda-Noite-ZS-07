0fc3dc0f443f2a9f2a554c4b35e5cd76c34b1e33
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-25 15:02:47 -0300

    Configure the adoptions views

--- apps/adoptions/views.py
+++ apps/adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Adoption
+from .serializer import AdoptionSerializer
 
 # Create your views here.
+
+class AdoptionViewSet(viewsets.ModelViewSet):
+    queryset = Adoption.objects.all()
+    serializer_class = AdoptionSerializer
