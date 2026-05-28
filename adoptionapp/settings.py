4b170298f903114e41fd0bd5574b620be0c06f4b
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-27 23:32:12 -0300

    Settings configuration

--- adoptionapp/settings.py
+++ adoptionapp/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'adoptionapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'staticfiles'), 
+]
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
