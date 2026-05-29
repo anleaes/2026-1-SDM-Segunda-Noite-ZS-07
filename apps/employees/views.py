cfd7b9e2f1d68f5d8d71c04b2f62e909059dcbd3
thaisgmaier <thaisgmaier@outlook.com>
2026-05-29 19:32:55 -0300

    adding employees views

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Employee
+from .forms import EmployeeForm
 from rest_framework import viewsets
 from .serializer import EmployeeSerializer
 
 # Create your views here.
-
-
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+def add_employee(request):
+    template_name = 'employees/add_employee.html'
+    context = {}
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('employees:list_employees')
+    form = EmployeeForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+def list_employees(request):
+    template_name = 'employees/list_employees.html'
+    employees = Employee.objects.filter()
+    context = {
+        'employees': employees,
+    }
+    return render(request, template_name, context)
+
+def edit_employee(request, id_employee):
+    template_name = 'employees/add_employee.html'
+    context ={}
+    employee = get_object_or_404(Employee, id=id_employee)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employees:list_employees')
+    form = EmployeeForm(instance=employee)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_employee(request, id_employee):
+    employee = Employee.objects.get(id=id_employee)
+    employee.delete()
+    return redirect('employees:list_employees')
