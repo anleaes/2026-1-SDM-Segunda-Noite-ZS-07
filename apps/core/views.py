cce5d8560b407761d3b25117967083655ddbfabf
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-05-29 10:37:26 -0300

    Configure the home page with data from the back-end and add data to the database.

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from animals.models import Animal
 
 # Create your views here.
 
 def home(request):
     template_name ='core/home.html'
-    context = {}
+    animals = Animal.objects.filter(adopted=False).prefetch_related('characteristic')
+    context = {
+        'animals': animals
+    }
     return render(request, template_name, context)
