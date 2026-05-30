e4ae83791fe386a03f6ecb6d0ae52a8ed6ffca2f
thaisgmaier <thaisgmaier@outlook.com>
2026-05-29 21:38:49 -0300

    fixing bugs

--- apps/adopters/urls.py
+++ apps/adopters/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 #    path('', include(router.urls) ),
     path('listar/', views.list_adopters, name='list_adopters'),
     path('adicionar/', views.add_adopter, name='add_adopter'),
-    path('editar/<int:register>/', views.edit_adopter, name='edit_adopter'),
-    path('excluir/<int:register>/', views.delete_adopter, name='delete_adopter'),
+    path('editar/<int:register_adopter>/', views.edit_adopter, name='edit_adopter'),
+    path('excluir/<int:register_adopter>/', views.delete_adopter, name='delete_adopter'),
 ]
