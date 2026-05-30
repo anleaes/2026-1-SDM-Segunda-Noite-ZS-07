d071b95fc68d7632f15bb7957209007bb57ed19a
thaisgmaier <thaisgmaier@outlook.com>
2026-05-29 21:11:28 -0300

    creating employees templates

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 
 app_name = 'employees'
 
-router = routers.SimpleRouter()
-router.register('', views.EmployeeViewSet, basename='funcionarios')
+#router = routers.SimpleRouter()
+#router.register('', views.EmployeeViewSet, basename='funcionarios')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
     path('listar/', views.list_employees, name='list_employees'),
     path('adicionar/', views.add_employee, name='add_employee'),
-    path('editar/<int:id_employee>/', views.edit_employee, name='edit_employee'),
-    path('excluir/<int:id_employee>/', views.delete_employee, name='delete_employee'),
+    path('editar/<int:register_employee>/', views.edit_employee, name='edit_employee'),
+    path('excluir/<int:register_employee>/', views.delete_employee, name='delete_employee'),
 ]
--- apps/employees/views.py
+++ apps/employees/views.py
@@ -5,9 +5,9 @@ from rest_framework import viewsets
 from .serializer import EmployeeSerializer
 
 # Create your views here.
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+#class EmployeeViewSet(viewsets.ModelViewSet):
+#    queryset = Employee.objects.all()
+#    serializer_class = EmployeeSerializer
 
 def add_employee(request):
     template_name = 'employees/add_employee.html'
@@ -31,10 +31,10 @@ def list_employees(request):
     }
     return render(request, template_name, context)
 
-def edit_employee(request, id_employee):
+def edit_employee(request, register_employee):
     template_name = 'employees/add_employee.html'
     context ={}
-    employee = get_object_or_404(Employee, id=id_employee)
+    employee = get_object_or_404(Employee, register=register_employee)
     if request.method == 'POST':
         form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
@@ -44,7 +44,7 @@ def edit_employee(request, id_employee):
     context['form'] = form
     return render(request, template_name, context)
 
-def delete_employee(request, id_employee):
-    employee = Employee.objects.get(id=id_employee)
+def delete_employee(request, register_employee):
+    employee = Employee.objects.get(register=register_employee)
     employee.delete()
     return redirect('employees:list_employees')
