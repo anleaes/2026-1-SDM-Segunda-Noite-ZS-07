0d48daabb167e3664530a6bb663674e1ccb6aa6c
thaisgmaier <thaisgmaier@outlook.com>
2026-05-29 21:31:12 -0300

    creating adopters templates

--- apps/adopters/urls.py
+++ apps/adopters/urls.py
@@ -4,9 +4,13 @@ from rest_framework import routers
 
 app_name = 'adopters'
 
-router = routers.SimpleRouter()
-router.register('', views.AdopterViewSet, basename='adotantes')
+# router = routers.SimpleRouter()
+# router.register('', views.AdopterViewSet, basename='adotantes')
 
 urlpatterns = [
-    path('', include(router.urls) )
+#    path('', include(router.urls) ),
+    path('listar/', views.list_adopters, name='list_adopters'),
+    path('adicionar/', views.add_adopter, name='add_adopter'),
+    path('editar/<int:register>/', views.edit_adopter, name='edit_adopter'),
+    path('excluir/<int:register>/', views.delete_adopter, name='delete_adopter'),
 ]
