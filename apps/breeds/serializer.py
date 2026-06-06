d326128420febe9c41743db0a649438b6e24af22
thaisgmaier <thaisgmaier@outlook.com>
2026-06-05 22:23:03 -0300

    adjusting breed serializer to accept specie by id

--- apps/breeds/serializer.py
+++ apps/breeds/serializer.py
@@ -1,8 +1,8 @@
-from .models import Breed
+from .models import Breed, Specie
 from rest_framework import serializers
 
 class BreedSerializer(serializers.ModelSerializer):
-    specie = serializers.StringRelatedField()
+    specie = serializers.PrimaryKeyRelatedField(queryset=Specie.objects.all())
     class Meta:
         model = Breed
         fields = '__all__'
