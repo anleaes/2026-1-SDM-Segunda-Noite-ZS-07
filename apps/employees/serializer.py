ad657a849fc0637538acd5fd855f3d12596385be
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-06-09 13:45:37 -0300

    adjusts the level of employee access to the system.

--- apps/employees/serializer.py
+++ apps/employees/serializer.py
@@ -2,7 +2,16 @@ from .models import Employee
 from rest_framework import serializers
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    role = serializers.SerializerMethodField()
     username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = Employee
         fields = '__all__'
+
+    def get_role(self, obj):
+        if obj.user:
+            if obj.user.is_superuser and obj.user.is_staff:
+                return 'Administrador'
+            elif obj.user.is_staff:
+                return 'Moderador'
+        return 'user'
